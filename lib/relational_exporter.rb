a7d4702a9e04fc84018cd7491c246bba0d507d07
Andrew Hammond <andrew@evertrue.com>
2014-02-19 11:33:08 -0500

    Add basic benchmarking, add configuration for pool size and record limit

--- lib/relational_exporter.rb
+++ lib/relational_exporter.rb
@@ -1,5 +1,6 @@
 require 'byebug'
 require 'csv'
+require 'benchmark'
 require 'hashie'
 require 'relational_exporter/version'
 require 'relational_exporter/active_record_extension'
@@ -27,40 +28,65 @@ module RelationalExporter
       load_models
     end
 
-    def export(output_config, &block)
+    def export(options, &block)
       ActiveRecord::Base.logger = @logger
       Celluloid.logger = @logger
 
-      output_config = Hashie::Mash.new output_config
+      options = Hashie::Mash.new options
 
-      main_klass = output_config.output.model.to_s.classify.constantize
+      main_klass = options.output.model.to_s.classify.constantize
 
-      main_klass.set_scope_from_hash output_config.output.scope.as_json
+      main_klass.set_scope_from_hash options.output.scope.as_json
 
-      csv_builder = RelationalExporter::CsvBuilder.new output_config.file_path
+      total_records = main_klass.find_all_by_scope(options.output.scope.as_json).count
+      remaining_records = total_records
+
+      csv_builder = RelationalExporter::CsvBuilder.new options.file_path
       Celluloid::Actor[:csv_builder] = csv_builder
       result = csv_builder.future.start
-      pool = RelationalExporter::RecordWorker.pool size: 8
+      pool_size = options.workers || 10
+      pool = RelationalExporter::RecordWorker.pool(size: pool_size)
       get_headers = true
 
       record_sequence = -1
-      main_klass.find_all_by_scope(output_config.output.scope.as_json).find_in_batches(batch_size: 100) do |records|
-        records.each do |record|
-          record_sequence += 1
+      batch_count = 0
 
-          args = [record_sequence, record, output_config.output.associations, get_headers]
-          if get_headers
-            pool.get_csv_row(*args)
-            get_headers = false
-          else
-            pool.async.get_csv_row(*args)
+      batch_options = Hashie::Mash.new({batch_size: 100}.merge(options.batch_options || {}))
+      limit = options.limit.to_i || nil
+
+      @logger.info "CSV export will process #{limit} of #{total_records} total records."
+
+      all_bm = Benchmark.measure do
+        catch(:hit_limit) do
+          main_klass.find_all_by_scope(options.output.scope.as_json).find_in_batches(batch_options.to_h.symbolize_keys) do |records|
+            batch_count+=1
+            batch_bm = Benchmark.measure do
+              records.each do |record|
+                record_sequence += 1
+                remaining_records -= 1
+
+                args = [record_sequence, record, options.output.associations, get_headers]
+                if get_headers
+                  pool.get_csv_row(*args)
+                  get_headers = false
+                else
+                  pool.async.get_csv_row(*args)
+                end
+
+                throw :hit_limit if record_sequence == limit
+              end
+            end
+
+            @logger.debug "Batch of #{records.size} queued. #{remaining_records} remaining. Benchmark: #{batch_bm}"
           end
         end
+
+        csv_builder.end_index = record_sequence
+
+        @logger.info "CSV export complete <#{options.file_path}>" if result.value === true
       end
 
-      csv_builder.end_index = record_sequence
-
-      @logger.info "CSV export complete" if result.value === true
+      @logger.debug "#{batch_count} batches processed. Benchmark: #{all_bm}"
 
       pool.terminate
       csv_builder.terminate
